babd91d97f34976c8cf1313ad2d2b49ab54441bb
Evan Johns <43017805+CutlassS1968@users.noreply.github.com>
2022-04-25 02:47:52 -0400

    I ACTUALLY FIXED THE DISTORTION

--- main.py
+++ main.py
@@ -4,16 +4,27 @@ import pygame
 import math
 
 # IDEA: Grayscale unless color is majority red
-# TODO: Fix ray cone not including rays outside 360 degrees
 
 WIDTH = 1920
 HEIGHT = 1080
-RAY_COUNT = 1920
-FOV = 60 * (math.pi / 180)
+# Field of the camera's view
+FOV = 130 * (math.pi / 180)
+# Width of each line drawn to the screen
+RES_SCALE = 5
+# Number of rays in fov_cone scale based on size of window
+RAY_COUNT = int((WIDTH / (FOV / (360 * (math.pi/180)))) / RES_SCALE)
+# Amount the user rotates their fov
 ROTATION_UNITS = 2 * (math.pi / 180)
-MOVE_UNITS = 3
+# Amount the user moves in the world
+MOVE_UNITS = 5
+# Default of each ray (View Distance)
 GR = math.sqrt(math.pow(HEIGHT, 2) + math.pow(WIDTH, 2))
+# Distance till light = 0
 LIGHT_DIST = GR
+# CAMERA_PLANE_DIST = math.fabs((WIDTH / 2) / math.tan((FOV * 180/math.pi) / 2))
+CAMERA_PLANE_DIST = (WIDTH/2) / (math.tan(FOV/2))
+
+WALL_HEIGHT = 1000
 
 colors = {
     "RED": (255, 0, 0),
@@ -45,6 +56,9 @@ class Line:
     def get_rd(self):
         return self.rd
 
+    def set_rd(self, rd):
+        self.rd = rd
+
     def get_p1(self):
         return self.p1
 
@@ -75,14 +89,20 @@ class Line:
 
 class Engine:
     def __init__(self):
-        self.current_position = WIDTH / 2, HEIGHT / 2
+        self.current_position = WIDTH / 2, 10
         self.walls = []
         self.all_rays = []
+        # All rays that are to be displayed
+        self.display_rays = []
+        self.camera_plane = Line((0, 0), (0, 0), colors["RED"], 0)
         self.gen_walls()
         self.gen_rays()
 
-        self.ray_cone = []
-        self.rotation_delta = 90 * (math.pi / 180)
+        # All rays that are located inside the FOV cone
+        self.cone_rays = []
+        # All rays inside the FOV cone adjusted for camera plane
+        self.camera_rays = []
+        self.rotation_delta = (90 * (math.pi / 180)) - (FOV/2)
         self.fov_lower = self.rotation_delta
         self.fov_upper = self.rotation_delta + FOV
 
@@ -92,6 +112,7 @@ class Engine:
         self.fov_center_ray = Line(p1, (x, y), colors["WHITE"], 0)
 
         self.update_cone()
+        self.update_camera_plane()
 
     def gen_walls(self):
         wall_width = 5
@@ -126,52 +147,127 @@ class Engine:
             self.all_rays[i].set_p2(p2)
 
     def update_cone(self):
-        self.ray_cone.clear()
+        self.cone_rays.clear()
         self.fov_lower = self.rotation_delta
 
         self.fov_upper = self.rotation_delta + FOV
         if self.fov_upper > (360 * (math.pi/180)):
             self.fov_upper = (self.rotation_delta + FOV) - (360 * (math.pi/180))
 
+        p1 = self.current_position
+        x = p1[0] + (GR * math.cos((FOV / 2) + self.rotation_delta))
+        y = p1[1] + (GR * math.sin((FOV / 2) + self.rotation_delta))
+        self.fov_center_ray.set_rd((FOV / 2) + self.rotation_delta)
+        self.fov_center_ray.set_p2((x, y))
+        self.fov_center_ray.set_p1(self.current_position)
+
         if self.fov_upper > self.fov_lower:
             for i in range(RAY_COUNT):
                 rd = self.all_rays[i].get_rd()
                 if self.fov_lower <= rd <= self.fov_upper:
-                    self.ray_cone.append(self.all_rays[i])
+                    self.cone_rays.append(self.all_rays[i])
         if self.fov_lower > self.fov_upper:
             for i in range(RAY_COUNT):
                 rd = self.all_rays[i].get_rd()
                 if self.fov_lower < rd < (360 * (math.pi / 180)):
-                    self.ray_cone.append(self.all_rays[i])
+                    self.cone_rays.append(self.all_rays[i])
             for i in range(RAY_COUNT):
                 rd = self.all_rays[i].get_rd()
                 if rd < self.fov_lower and rd < self.fov_upper:
-                    self.ray_cone.append(self.all_rays[i])
+                    self.cone_rays.append(self.all_rays[i])
+
+    def update_camera_plane(self):
+        r1 = self.cone_rays[0]
+        r2 = self.cone_rays[len(self.cone_rays) - 1]
+
+        x1 = self.current_position[0] + (CAMERA_PLANE_DIST * math.cos(r1.get_rd()))
+        y1 = self.current_position[1] + (CAMERA_PLANE_DIST * math.sin(r1.get_rd()))
+
+        x2 = self.current_position[0] + (CAMERA_PLANE_DIST * math.cos(r2.get_rd()))
+        y2 = self.current_position[1] + (CAMERA_PLANE_DIST * math.sin(r2.get_rd()))
+
+        p1 = x1, y1
+        p2 = x2, y2
+
+        # print("----------------------------------------------------------------------------------")
+        #
+        # print("Angles: ", r1.get_rd() * 180 / math.pi, r2.get_rd() * 180 / math.pi)
+        #
+        # print("Plane Dist: ", CAMERA_PLANE_DIST)
+        #
+        # print("Current Pos: ", r1.get_p1()[0], r1.get_p1()[1], r2.get_p1()[0], r2.get_p1()[1])
+        #
+        # print("FOV____ Pos: ", x1, y1, x2, y2)
+
+        self.camera_plane = Line(p1, p2, colors["RED"], 0)
+
+    def update_camera(self):
+        # self.camera_rays = self.cone_rays.copy()
+        self.camera_rays.clear()
+        camera_plane_center = self.intersect(self.camera_plane, self.fov_center_ray)
+        # print("-----------------------------------")
+        # print(math.degrees(self.cone_rays[0].get_rd()))
+        for i in range(len(self.cone_rays)):
+            # angle from the fov center ray that intersects the camera plane at an even interval (110)
+
+            theta = math.atan(((self.camera_plane.get_length()/len(self.cone_rays)) * (i - ((len(self.cone_rays)-1)/2)))/CAMERA_PLANE_DIST)
+            print(math.degrees(theta))
+            # print(theta * 180/math.pi)
+            p1 = self.current_position
+            x = p1[0] + (GR * math.cos(self.fov_center_ray.get_rd() + theta))
+            y = p1[1] + (GR * math.sin(self.fov_center_ray.get_rd() + theta))
+            p2 = x, y
+            self.camera_rays.append(Line(p1, p2, self.cone_rays[i].get_color(), self.fov_center_ray.get_rd() + theta))
+        # print(len(self.camera_rays))
+
+        # Project rays at even intervals on the plane and  display these rays
+        # TODO: THIS IS WRONG, somehow? idk what the answer to this crap is
+        # self.display_rays.clear()
+        # for i in range(len(self.camera_rays)):
+        #     p1 = self.intersect(self.camera_rays[i], self.camera_plane)
+        #     # draw line parallel to center_cone_ray from collision point with camera plane
+        #     x = p1[0] + (GR * math.cos((FOV / 2) + self.rotation_delta))
+        #     y = p1[1] + (GR * math.sin((FOV / 2) + self.rotation_delta))
+        #     p2 = x, y
+        #     ray = Line(p1, p2, self.camera_rays[i].get_color(), self.camera_rays[i].get_rd())
+        #     self.display_rays.append(ray)
+
 
     def update(self):
-        # print(len(self.ray_cone), ", ", len(self.all_rays))
+        # print(len(self.cone_rays), ", ", len(self.all_rays))
         self.update_rays()
         self.update_cone()
-        self.check_collisions(self.ray_cone)
+        self.update_camera_plane()
+        self.update_camera()
+        # self.check_collisions(self.display_rays)
+        self.check_collisions(self.camera_rays)
 
     def draw(self, screen):
-        padding = 360 / (FOV * (180/math.pi))
-        padding = WIDTH/len(self.ray_cone)
+        displayed_rays = self.camera_rays.copy()
+        padding = WIDTH/len(displayed_rays)
         screen.fill(colors["BLACK"])
+        pygame.draw.rect(screen, (100, 100, 100), ((0, 0), (WIDTH, HEIGHT/2)))
+        pygame.draw.rect(screen, (75, 75, 75), ((0, HEIGHT/2), (WIDTH, HEIGHT)))
 
-        for i in range(len(self.ray_cone)):
+        for i in range(len(displayed_rays)):
             hw1 = Line((0, 0), (0, 0), colors["BLACK"], 0)
             hw2 = Line((0, 0), (0, 0), colors["BLACK"], 0)
 
-            length = self.ray_cone[i].get_length()
-            color = self.depth_shader(self.ray_cone[i].get_color(), length)
-            new_length = (((length / GR) * HEIGHT) - HEIGHT) / 2
+            length = displayed_rays[i].get_length()
+            correct_distance = length * math.cos(math.fabs(displayed_rays[i].get_rd() - self.fov_center_ray.get_rd()))
+            color = self.depth_shader(displayed_rays[i].get_color(), length)
+
+            distance_to_projection_plane = CAMERA_PLANE_DIST
+            distance_to_slice = length * math.cos(math.fabs(displayed_rays[i].get_rd() - self.fov_center_ray.get_rd()))
+            # THIS LITERALLY TOOK LIKE 8 HOURS TO REALIZE I HAD THIS EQUATION WRONG
+            # https://permadi.com/1996/05/ray-casting-tutorial-9/
+            projected_slice_height = (WALL_HEIGHT / distance_to_slice) * distance_to_projection_plane
 
             hw1.set_p1((i * padding, HEIGHT / 2))
-            hw1.set_p2((i * padding, (HEIGHT / 2) + new_length))
+            hw1.set_p2((i * padding, (HEIGHT / 2) + projected_slice_height / 2))
 
             hw2.set_p1((i * padding, HEIGHT / 2))
-            hw2.set_p2((i * padding, (HEIGHT / 2) - new_length))
+            hw2.set_p2((i * padding, (HEIGHT / 2) - projected_slice_height / 2))
 
             hw1.set_color(color)
             hw2.set_color(color)
@@ -182,10 +278,11 @@ class Engine:
             hw1.draw(screen)
             hw2.draw(screen)
 
-        # for ray in self.ray_cone:
+        # for ray in displayed_rays:
         #     ray.draw(screen)
         # for wall in self.walls:
         #     wall.draw(screen)
+        # self.camera_plane.draw(screen)
 
     def depth_shader(self, col, l):
         c = []
@@ -296,6 +393,8 @@ class Game:
     def run(self):
         ps = []
         while self.running:
+            keys = pygame.key.get_pressed()
+            self.running = self.engine.move(keys)
             # Look through all events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -305,9 +404,6 @@ class Game:
                     if len(ps) > 1:
                         self.engine.add_wall(Line(ps[0], ps[1], rand_color(), 0))
                         ps.clear()
-
-            keys = pygame.key.get_pressed()
-            self.running = self.engine.move(keys)
 
             # Update rays
             self.engine.update()
